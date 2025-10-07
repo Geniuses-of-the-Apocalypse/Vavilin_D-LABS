35e34bfcbdbe5718e94320d5cacb93971f2ab04e
Даниил <mrdandy0911@gmail.com>
2025-10-07 13:44:05 +0300

    Update LB-Z1.1.cpp
    Writing a code

--- LB-Z1.1.cpp
+++ LB-Z1.1.cpp
@@ -1,1 +1,38 @@
-C++
+#include <iostream>
+
+using namespace std;
+
+struct Brand
+{
+    double first;
+    int second;
+    
+    double cost()
+    {
+        return first * second;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    cout << "ТОВАРЫ." << endl;
+    cout << "===================" << endl;
+    
+    Brand item;
+    
+    double a;
+    int b;
+    
+    cout << "Введите не целое число: ";
+    cin >> a;
+    
+    cout << "Введите целое число: ";
+    cin >> b;
+    
+    item.first = a;
+    item.second = b;
+    
+    cout << "==============================================" << endl;
+    cout << "Общая стоимость товара: " << item.cost() << " рублей." << endl;
+    return 0;
+}
