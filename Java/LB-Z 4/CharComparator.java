c81d6a32c0def68ea3f9a4e0d35803194f8f802f
Даниил <mrdandy0911@gmail.com>
2026-03-14 02:42:12 +0300

    Update CharComparator.java

--- Java/LB-Z 4/CharComparator.java
+++ Java/LB-Z 4/CharComparator.java
@@ -1,8 +1,10 @@
 package JAVA.GUU.RU.LAB4;
 
-public class CharComparator implements Comparator<Character> {
+public class CharComparator implements Comparator<Character>
+{
     @Override
-    public int compare(Character a, Character b) {
+    public int compare(Character a, Character b)
+    {
         return Character.compare(a, b);
     }
 }
