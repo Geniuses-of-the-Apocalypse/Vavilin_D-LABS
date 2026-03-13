bcf958a154ae3bb3d4f764147d22e1433e17218f
Даниил <mrdandy0911@gmail.com>
2026-03-14 02:41:50 +0300

    Update and rename IntegerComporator.java to IntegerComparator.java

--- Java/LB-Z 4/IntegerComporator.java
+++
@@ -1,8 +0,0 @@
-package JAVA.GUU.RU.LAB4;
-
-public class IntegerComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer a, Integer b) {
-        return Integer.compare(a, b);
-    }
-}
