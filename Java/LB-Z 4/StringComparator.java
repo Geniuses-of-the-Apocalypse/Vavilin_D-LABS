527efd2377eb0efa4a97993865eb24b1154840f7
Даниил <mrdandy0911@gmail.com>
2026-03-14 02:41:22 +0300

    Update StringComparator.java

--- Java/LB-Z 4/StringComparator.java
+++ Java/LB-Z 4/StringComparator.java
@@ -1,8 +1,10 @@
 package JAVA.GUU.RU.LAB4;
 
-public class StringComparator implements Comparator<String> {
+public class StringComparator implements Comparator<String>
+{
     @Override
-    public int compare(String a, String b) {
+    public int compare(String a, String b) 
+    {
         return a.compareTo(b);
     }
 }
