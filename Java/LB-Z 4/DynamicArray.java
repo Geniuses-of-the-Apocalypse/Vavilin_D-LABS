934424aa0238684f6a1cb60f65f60e903fd21766
Даниил <mrdandy0911@gmail.com>
2026-03-15 16:41:20 +0300

    Update DynamicArray.java
    Внёс некоторые правки в оформление кода

--- Java/LB-Z 4/DynamicArray.java
+++ Java/LB-Z 4/DynamicArray.java
@@ -11,18 +11,17 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
     private Comparator<T> comparator;
 
     //Конструкторы
-    @SuppressWarnings("unchecked")
-    public DynamicArray(Comparator<T> comparator) {
+    public DynamicArray(Comparator<T> comparator)
+    {
         this.elements = (DataElement<T>[]) new DataElement[DEFAULT_CAPACITY];
         this.size = 0;
         this.comparator = comparator;
     }
 
-    @SuppressWarnings("unchecked")
     public DynamicArray(int initialCapacity, Comparator<T> comparator) {
-        if (initialCapacity < 0) {
+        if (initialCapacity < 0)
             throw new IllegalArgumentException("Capacity cannot be negative");
-        }
+
         this.elements = (DataElement<T>[]) new DataElement[initialCapacity];
         this.size = 0;
         this.comparator = comparator;
@@ -32,25 +31,21 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
 
     //Добавление элемента
     public void add(T data) {
-        if (size == elements.length) {
+        if (size == elements.length)
             resize();
-        }
         elements[size++] = new DataElement<>(data);
     }
 
     //Добавление элемента по индексу
     public void add(int index, T data) {
-        if (index < 0 || index > size) {
+        if (index < 0 || index > size)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
 
-        if (size == elements.length) {
+        if (size == elements.length)
             resize();
-        }
 
-        for (int i = size; i > index; i--) {
+        for (int i = size; i > index; i--)
             elements[i] = elements[i - 1];
-        }
 
         elements[index] = new DataElement<>(data);
         size++;
@@ -58,23 +53,20 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
 
     //Получение элемента по индексу
     public DataElement<T> get(int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index >= size)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
         return elements[index];
     }
 
     //Удаление элемента по индексу
     public DataElement<T> remove(int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index >= size)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
 
         DataElement<T> removed = elements[index];
 
-        for (int i = index; i < size - 1; i++) {
+        for (int i = index; i < size - 1; i++)
             elements[i] = elements[i + 1];
-        }
 
         elements[size - 1] = null;
         size--;
@@ -83,8 +75,10 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
     }
 
     //Удаление элемента по значению
-    public boolean remove(T data) {
-        for (int i = 0; i < size; i++) {
+    public boolean remove(T data)
+    {
+        for (int i = 0; i < size; i++)
+        {
             if (comparator.compare(elements[i].getData(), data) == 0) {
                 remove(i);
                 return true;
@@ -95,9 +89,8 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
 
     //Замена элемента
     public DataElement<T> set(int index, T data) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index >= size)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
 
         DataElement<T> old = elements[index];
         elements[index] = new DataElement<>(data);
@@ -105,40 +98,44 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
     }
 
     //Проверка наличия элемента
-    public boolean contains(T data) {
+    public boolean contains(T data)
+    {
         return indexOf(data) != -1;
     }
 
     //Поиск индекса элемента
-    public int indexOf(T data) {
-        for (int i = 0; i < size; i++) {
-            if (comparator.compare(elements[i].getData(), data) == 0) {
+    public int indexOf(T data)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (comparator.compare(elements[i].getData(), data) == 0)
                 return i;
-            }
         }
         return -1;
     }
 
     //Получение размера
-    public int size() {
+    public int size()
+    {
         return size;
     }
 
     //Проверка на пустоту
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return size == 0;
     }
 
     //Очистка массива
-    @SuppressWarnings("unchecked")
-    public void clear() {
+    public void clear()
+    {
         elements = (DataElement<T>[]) new DataElement[DEFAULT_CAPACITY];
         size = 0;
     }
 
     //Изменение размера массива
-    @SuppressWarnings("unchecked")
-    private void resize() {
+    private void resize()
+    {
         int newCapacity = elements.length * 2;
         DataElement<T>[] newArray = (DataElement<T>[]) new DataElement[newCapacity];
         System.arraycopy(elements, 0, newArray, 0, size);
@@ -146,42 +143,43 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
     }
 
     //Сортировка подсчётом (для целых чисел)
-    public void countingSort() {
+    public void countingSort()
+    {
         if (size <= 1) return;
 
-        if (!isIntegerType()) {
+        if (!isIntegerType())
             throw new UnsupportedOperationException("Counting sort supports only integer types");
-        }
 
         int min = getIntValue(elements[0].getData());
         int max = getIntValue(elements[0].getData());
 
-        for (int i = 1; i < size; i++) {
+        for (int i = 1; i < size; i++)
+        {
             int value = getIntValue(elements[i].getData());
-            if (value < min) {
+            if (value < min)
                 min = value;
-            }
-            if (value > max) {
+            if (value > max)
                 max = value;
-            }
         }
 
         int range = max - min + 1;
         int[] count = new int[range];
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             int value = getIntValue(elements[i].getData());
             count[value - min]++;
         }
 
-        for (int i = 1; i < range; i++) {
+        for (int i = 1; i < range; i++)
+        {
             count[i] += count[i - 1];
         }
 
-        @SuppressWarnings("unchecked")
         DataElement<T>[] output = (DataElement<T>[]) new DataElement[size];
 
-        for (int i = size - 1; i >= 0; i--) {
+        for (int i = size - 1; i >= 0; i--)
+        {
             int value = getIntValue(elements[i].getData());
             int index = count[value - min] - 1;
             output[index] = elements[i];
@@ -192,7 +190,8 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
     }
 
     //Проверка на целочисленность
-    private boolean isIntegerType() {
+    private boolean isIntegerType()
+    {
         if (size == 0) return false;
         T data = elements[0].getData();
         return data instanceof Integer ||
@@ -202,45 +201,49 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
     }
 
     //Получение целочисленного значения
-    private int getIntValue(T data) {
-        if (data instanceof Integer) {
+    private int getIntValue(T data)
+    {
+        if (data instanceof Integer)
             return (Integer) data;
-        } else if (data instanceof Long) {
+        else if (data instanceof Long)
             return ((Long) data).intValue();
-        } else if (data instanceof Short) {
+        else if (data instanceof Short)
             return ((Short) data).intValue();
-        } else if (data instanceof Byte) {
+        else if (data instanceof Byte)
             return ((Byte) data).intValue();
-        } else {
+        else
             throw new IllegalArgumentException("Cannot convert to int: " + data.getClass());
-        }
     }
 
     //Сортировка пузырьком (для любых типов)
-    public void bubbleSort() {
+    public void bubbleSort()
+    {
         for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
-                if (comparator.compare(elements[j].getData(), elements[j + 1].getData()) > 0) {
+                if (comparator.compare(elements[j].getData(), elements[j + 1].getData()) > 0)
                     swap(j, j + 1);
-                }
             }
         }
     }
 
     //Быстрая сортировка (для любых типов)
-    public void quickSort() {
+    public void quickSort()
+    {
         quickSort(0, size - 1);
     }
 
-    private void quickSort(int low, int high) {
-        if (low < high) {
+    private void quickSort(int low, int high)
+    {
+        if (low < high)
+        {
             int pi = partition(low, high);
             quickSort(low, pi - 1);
             quickSort(pi + 1, high);
         }
     }
 
-    private int partition(int low, int high) {
+    private int partition(int low, int high)
+    {
         T pivot = elements[high].getData();
         int i = low - 1;
 
@@ -255,47 +258,51 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
         return i + 1;
     }
 
-    private void swap(int i, int j) {
+    private void swap(int i, int j)
+    {
         DataElement<T> temp = elements[i];
         elements[i] = elements[j];
         elements[j] = temp;
     }
 
     //Бинарный поиск (нужен отсортированный массив)
-    public int binarySearch(T target) {
+    public int binarySearch(T target)
+    {
         int left = 0;
         int right = size - 1;
 
-        while (left <= right) {
+        while (left <= right)
+        {
             int mid = left + (right - left) / 2;
             int comparison = comparator.compare(elements[mid].getData(), target);
 
-            if (comparison == 0) {
+            if (comparison == 0)
                 return mid;
-            } else if (comparison < 0) {
+            else if (comparison < 0)
                 left = mid + 1;
-            } else {
+            else
                 right = mid - 1;
-            }
         }
-
         return -1;
     }
 
     //Линейный поиск
-    public int linearSearch(T target) {
-        for (int i = 0; i < size; i++) {
-            if (comparator.compare(elements[i].getData(), target) == 0) {
+    public int linearSearch(T target)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (comparator.compare(elements[i].getData(), target) == 0)
                 return i;
-            }
         }
         return -1;
     }
 
     //Итератор (for-each)
     @Override
-    public Iterator<DataElement<T>> iterator() {
-        return new Iterator<DataElement<T>>() {
+    public Iterator<DataElement<T>> iterator()
+    {
+        return new Iterator<DataElement<T>>()
+        {
             private int currentIndex = 0;
 
             @Override
@@ -304,10 +311,10 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
             }
 
             @Override
-            public DataElement<T> next() {
-                if (!hasNext()) {
+            public DataElement<T> next()
+            {
+                if (!hasNext())
                     throw new NoSuchElementException();
-                }
                 return elements[currentIndex++];
             }
         };
@@ -315,15 +322,14 @@ public class DynamicArray<T> implements Iterable<DataElement<T>> {
 
     //Метод toString
     @Override
-    public String toString() {
-        if (size == 0) {
+    public String toString()
+    {
+        if (size == 0)
             return "[]";
-        }
 
         StringBuilder sb = new StringBuilder("[");
-        for (int i = 0; i < size - 1; i++) {
+        for (int i = 0; i < size - 1; i++)
             sb.append(elements[i]).append(", ");
-        }
         sb.append(elements[size - 1]).append("]");
 
         return sb.toString();
