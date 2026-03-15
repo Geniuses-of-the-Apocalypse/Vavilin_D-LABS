605576887707057ddc3b7440446455125edc1006
Даниил <mrdandy0911@gmail.com>
2026-03-15 22:59:13 +0300

    Create FileReader.java

---
+++ Java/LB-Z 5/FileReader.java
@@ -0,0 +1,25 @@
+package JAVA.GUU.RU.LAB5;
+
+import java.io.*;
+import java.nio.file.*;
+
+public class FileReader
+{
+    public static String readFile(String filePath) throws IOException
+    {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    public static boolean fileExists(String filePath)
+    {
+        return Files.exists(Paths.get(filePath));
+    }
+
+    public static String getFileExtension(String filePath)
+    {
+        int lastDot = filePath.lastIndexOf('.');
+        if (lastDot > 0)
+            return filePath.substring(lastDot + 1);
+        return "";
+    }
+}
