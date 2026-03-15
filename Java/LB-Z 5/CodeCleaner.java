366574098ba28aac85c7202a89fdc82c85525ed6
Даниил <mrdandy0911@gmail.com>
2026-03-15 22:59:45 +0300

    Create CodeCleaner.java

---
+++ Java/LB-Z 5/CodeCleaner.java
@@ -0,0 +1,104 @@
+/*
+charAt() - возвращает символ по индексу в строке
+compile - ?
+IOException - проверяемое исключение, которое возникает при неудачной операции ввода-вывода
+printf - форматирования текста и переменных для вывода
+ */
+
+package JAVA.GUU.RU.LAB5;
+
+//Класс для отчистки
+public class CodeCleaner
+{
+    //Удаление комментариев и литералов
+    public static String killCommentsNStrings(String code)
+    {
+        code = removeStringLiterals(code); //Строковые литералы в расход
+        code = removeCharLiterals(code); //Символьные литералы в расход
+        code = removeMultiComments(code); //Многострочныйе комментарии в расход
+        code = removeSingleComments(code); //Однострочные комментарии в расход
+
+        return code;
+    }
+
+    private static String removeStringLiterals(String code)
+    {
+        StringBuilder result = new StringBuilder();
+        boolean inString = false;
+
+        for (int i = 0; i < code.length(); i++)
+        {
+            char c = code.charAt(i);
+
+            if (c == '"' && (i == 0 || code.charAt(i - 1) != '\\'))
+            {
+                inString = !inString;
+                result.append('"');
+            }
+            else
+                result.append(c);
+        }
+        return result.toString();
+    }
+
+    private static String removeCharLiterals(String code)
+    {
+        StringBuilder result = new StringBuilder();
+        boolean inChar = false;
+
+        for (int i = 0; i < code.length(); i++)
+        {
+            char c = code.charAt(i);
+
+            if (c == '\'' && (i == 0 || code.charAt(i - 1) != '\\'))
+            {
+                inChar = !inChar;
+                result.append('\'');
+            }
+            else if (!inChar)
+                result.append(c);
+        }
+        return result.toString();
+    }
+
+    private static String removeMultiComments(String code)
+    {
+        StringBuilder result = new StringBuilder();
+        boolean inComment = false;
+
+        for (int i = 0; i < code.length(); i++)
+        {
+            if (!inComment && i < code.length() - 1 && code.charAt(i) == '*' && code.charAt(i + 1) == '/')
+            {
+                inComment = false;
+                i++;
+            }
+            else if (!inComment)
+                result.append(code.charAt(i));
+        }
+        return result.toString();
+    }
+
+    private static String removeSingleComments(String code)
+    {
+        StringBuilder result = new StringBuilder();
+        boolean inComment = false;
+
+        for (int i = 0; i < code.length(); i++)
+        {
+            if (!inComment && i < code.length() - 1 && code.charAt(i) == '/' && code.charAt(i + 1) == '/')
+            {
+                inComment = true;
+                i++;
+            }
+            else if (inComment && code.charAt(i) == '\n')
+            {
+                inComment = false;
+                result.append('\n');
+            }
+            else if (!inComment)
+                result.append(code.charAt(i));
+        }
+        return result.toString();
+    }
+}
