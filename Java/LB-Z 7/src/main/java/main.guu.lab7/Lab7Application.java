4019ce05215c7af8a7c9e98598a6daf782bf6bd5
Даниил <mrdandy0911@gmail.com>
2026-03-30 11:37:10 +0300

    Create Lab7Application.java
    Main проекта

---
+++ Java/LB-Z 7/src/main/java/main.guu.lab7/Lab7Application.java
@@ -0,0 +1,21 @@
+package main.guu.ru.lab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import main.guu.ru.lab7.component.AppStart;
+
+@SpringBootApplication
+public class Lab7Application {
+    private static ConfigurableApplicationContext context;
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Lab7Application.class);
+        app.setBanner((environment, sourceClass, out) -> out.print("┏━╸┏━┓┏━┓┏━┓╻ ╻   ┏━┓┏━┓╺┳╸╻ ╻   ┏━╸╻┏┓╻╺┳┓┏━╸┏━┓\n" +
+                "┃╺┓┣┳┛┣━┫┣━┛┣━┫   ┣━┛┣━┫ ┃ ┣━┫   ┣╸ ┃┃┗┫ ┃┃┣╸ ┣┳┛\n" +
+                "┗━┛╹┗╸╹ ╹╹  ╹ ╹   ╹  ╹ ╹ ╹ ╹ ╹   ╹  ╹╹ ╹╺┻┛┗━╸╹┗╸\n".toUpperCase()));
+        context = app.run(args);
+        AppStart appStart = context.getBean(AppStart.class);
+        appStart.start();
+    }
+}
