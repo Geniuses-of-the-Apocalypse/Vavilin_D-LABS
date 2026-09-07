2834625cb54260272652e4f441e2ccdd19df652e
theDandy222 <mrdandy0911@gmail.com>
2026-09-07 22:20:10 +0300

    Update and rename Lab1.py to LB 1.py

--- Functional_Programming/LB 1.py
+++ Functional_Programming/LB 1.py
@@ -3,30 +3,29 @@ from typing import Optional
 
 CSV_FILE = """\
 item,quantity,price
-tank,14,1020.3
-plane,18,305.2
-car,2,26.7
+tank,14,1020.5
+plane,19,306.4
+car,10,124.3
+tractor,24,250.3
 """
 
-# 1
+# ===( 1 )===
 def parse_csv(data: str) -> list[dict]:
     return list(csv.DictReader(data.splitlines()))
 
-# 2
+# ===( 2 )===
 def compute_revenue(rows: list[dict]) -> float:
     return sum(int(row["quantity"]) * float(row["price"]) for row in rows)
 
-# 3
+# ===( 3 )===
 def top_item(rows: list[dict]) -> Optional[dict]:
     return max(rows, key=lambda row: int(row["quantity"]) * float(row["price"]), default=None)
 
-# main
+# === main ===
 rows = parse_csv(CSV_FILE)
 
-print("Исходные данные: ")
-print(rows)
+print(">>Исходные данные: ", rows)
 
-print("\n Выручка: ", compute_revenue(rows))
+print("\n>>Выручка: ", compute_revenue(rows))
 
-print("\n Макс. выручка: ")
-print(top_item(rows))
+print("\n>>Максимальная выручка: ", top_item(rows))
