f721bdb4a648d2ec17eb4706b218bdd12acff015
theDandy222 <mrdandy0911@gmail.com>
2026-09-21 11:04:55 +0300

    Update LB 1.3.py
    Замена перемены rows.

--- Functional_Programming/LB 1.3.py
+++ Functional_Programming/LB 1.3.py
@@ -14,11 +14,11 @@ def parse_csv(data: str) -> list[dict]:
     return list(csv.DictReader(data.splitlines()))
 
 # ===( 2 )===
-def compute_revenue(rows: list[dict]) -> float:
-    return sum(map(lambda x:float(x["quantity"]) * float(x["price"]), rows))
+def compute_revenue(parse_csv(CSV_FILE): list[dict]) -> float:
+    return sum(map(lambda x:float(x["quantity"]) * float(x["price"]), parse_csv(CSV_FILE)))
 
 # ===( 3 )===
-def top_item(rows: list[dict]) -> Optional[dict]:
+def top_item(parse_csv(CSV_FILE): list[dict]) -> Optional[dict]:
     def rev(x: dict) -> float:
         return float(x["quantity"]) * float(x["price"])
     def go(items: list[dict]) -> Optional[dict]:
@@ -28,13 +28,13 @@ def top_item(rows: list[dict]) -> Optional[dict]:
             return items[0]
         best_of_tail = go(items[1:])
         return items[0] if rev(items[0]) >= rev(best_of_tail) else best_of_tail
-    return go(rows)
+    return go(parse_csv(CSV_FILE))
 
 # main
 rows = parse_csv(CSV_FILE)
 
-print("\n>>Исходные данные:", rows)
+print("\n>>Исходные данные:", parse_csv(CSV_FILE))
 
-print("\n>>Выручка:", compute_revenue(rows))
+print("\n>>Выручка:", compute_revenue(parse_csv(CSV_FILE)))
 
-print("\n>>Максимальная выручка:", top_item(rows))
+print("\n>>Максимальная выручка:", top_item(parse_csv(CSV_FILE)))
